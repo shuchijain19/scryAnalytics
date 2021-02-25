fc198e53a34228575fc92e3c816e37bea1f75be7
shuchi <shuchi19.jain@gmail.com>
2021-02-25 13:43:24 +0530

    UI implementation

---
+++ src/app/modules/home/home.module.ts
@@ -0,0 +1,21 @@
+import {NgModule} from "@angular/core";
+import { CommonModule } from '@angular/common';
+import {HomeComponent} from "./home.component"
+import {HomeRoutingModule} from "./home-routing.module";
+
+
+@NgModule({
+  declarations: [
+    HomeComponent
+  ],
+  imports: [
+    CommonModule,
+    HomeRoutingModule,
+  ],
+  exports: [],
+  providers: []
+
+})
+export class HomeModule {
+
+}
